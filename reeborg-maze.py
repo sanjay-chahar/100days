3bf0410b66863bd92001a862a71c844c10ff5dc8
Sanjay Chahar <chaharsan@yahoo.com>
2021-04-01 14:31:51 +0100

    Create reeborg-maze.py

--- reeborg-maze.py
+++ reeborg-maze.py
@@ -2,11 +2,6 @@ def turn_right():
     turn_left()
     turn_left()
     turn_left()
-def jump():
-    if wall_in_front and wall_on_right():
-        turn_left()
-    if  front_is_clear() and right_is_clear():
-        turn_right()
 while not at_goal():
     if right_is_clear():
         turn_right()
